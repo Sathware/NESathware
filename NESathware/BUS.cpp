9f8ce3799a8781adf795651f28c9b1e25dc5ecd8
Sathvik Chereddy <56492353+Sathware@users.noreply.github.com>
2022-08-26 03:46:39 -0400

    Cleaned up ROM access logic, removed Cartridge class indirection to ROM, and imposed made reads and writes explicit in CPU class

--- NESathware/BUS.cpp
+++ NESathware/BUS.cpp
@@ -1,6 +1,6 @@
 #include "BUS.h"
 
-ubyte& BUS::ReadCPU(ubyte2 address)
+ubyte BUS::ReadCPU(ubyte2 address)
 {
 	assert(address >= 0 && address <= 0xffff);//Sanity check
 	if (address < 0x2000)
@@ -26,12 +26,36 @@ ubyte& BUS::ReadCPU(ubyte2 address)
 	else if (address <= 0xffff)
 	{
 		//Cartridge space
-		return mpCartridge->Read(address);
+		return mpCartridge->ReadCPU(address);
 	}
 }
 
 void BUS::WriteCPU(ubyte val, ubyte2 address)
 {
-	//Temporary
-	ReadCPU(address) = val;
+	assert(address >= 0 && address <= 0xffff);//Sanity check
+	if (address < 0x2000)
+	{
+		//2KB internal RAM and mirrors
+		ubyte2 index = address % 0x0800u;
+		mRAM.at(index) = val;
+	}
+	else if (address < 0x4000)
+	{
+		//PPU registers and mirrors
+		ubyte2 index = address % 0x8u;
+	}
+	else if (address < 0x4018)
+	{
+		//APU and I/O registers
+	}
+	else if (address < 0x4020)
+	{
+		//Disabled
+		throw std::runtime_error("Tried to read disabled APU and I/O address");
+	}
+	else if (address <= 0xffff)
+	{
+		//Cartridge space
+		mpCartridge->WriteCPU(val, address);
+	}
 }
--- NESathware/BUS.h
+++ NESathware/BUS.h
@@ -3,7 +3,7 @@
 #include "CPU_6052.h"
 #include "PPU_2C02.h"
 #include "APU_2A03.h"
-#include "Cartridge.h"
+#include "Mapper.h"
 #include <array>
 
 //Handles inter-component communication
@@ -12,14 +12,14 @@ class BUS
 public:
 	//Maps data appropriately to the various components depending on the address
 	//Source: "https://www.nesdev.org/wiki/CPU_memory_map"
-	ubyte& ReadCPU(ubyte2 address);
+	ubyte ReadCPU(ubyte2 address);
 	void WriteCPU(ubyte val, ubyte2 address);
 public:
 
 	CPU_6052* mpCPU = nullptr;
 	PPU_2C02* mpPPU = nullptr;
 	APU_2A03* mpAPU = nullptr;
-	Cartridge* mpCartridge = nullptr;
+	Mapper* mpCartridge = nullptr;
 
 	//2KB onboard ram and rest of address space
 	std::array<ubyte, 0x0800u> mRAM = { 0 };
